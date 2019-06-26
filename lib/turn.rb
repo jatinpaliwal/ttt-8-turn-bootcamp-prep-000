25e81752e0b7d875b4e7a86fa24259adf908a67a
Jatin Paliwal <jatinpaliwal181@gmail.com>
2019-06-26 12:55:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,21 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def valid_move?(board,position)
+  if position.between?(0,8)
+    if position_taken?(board,position)== false
+      return true
+    else
+      return false
+    end
+  end
+end
+
+def position_taken?(board,position)
+  if board[position]==" " || board[position]== "" || board[position]== nil
+    return false
+  else
+    return true
+  end
+end
